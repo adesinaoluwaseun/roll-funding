f810f218af99e90457eef72d6c3af5581bc3d564
[adesinaoluwaseun] <[adesinaoluwaseun001@yahoo.com]>
2019-07-11 08:42:39 +0100

    service page added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// when the user click the popup div
+function myFunction() {
+    const popup = document.getElementById("mypopup");
+    popup.classList.toggle("show");
+}
+function myFunction1() {
+    const popup1 = document.getElementById("mypopup1");
+    popup1.classList.toggle("show");
+}
